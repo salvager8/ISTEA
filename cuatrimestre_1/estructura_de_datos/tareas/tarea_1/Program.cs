9889c3de60cf6df9791a50a8cd7d2ebcab4a1e0f
Salvador Gonzalez <salvager8@gmail.com>
2021-03-29 03:57:35 -0300

    Adding classes and two friday homeworks

---
+++ cuatrimestre_1/estructura_de_datos/tareas/tarea_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace tarea_1
+{
+	class Program
+	{
+		static int? GetNumberInput(int? valorEntrada) {
+			while (valorEntrada == null) {
+				try {
+					Console.Write("\n\nIngrese un valor de hasta 4 digitos: ");
+					valorEntrada = Convert.ToInt32(Console.ReadLine());
+				} catch {
+					Console.Write("El texto ingresado no es un numero, intente nuevamente");
+					Console.Write("\n\nIngrese un valor de hasta 4 digitos: ");
+				}
+			}
+			return valorEntrada;
+		}
+		static void Main(string[] args)
+		{
+			// Enunciado
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("\nEnunciado: \n");
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine("\tCrear una aplicación que permita el ingreso por pantalla de un numero de hasta 4 cifras");
+			Console.WriteLine("\ty devuelve si es par o impar, el proceso se debe repetir hasta que se ingrese el valor 0.");
+			Console.ResetColor();
+
+			int? valorEntrada = null;
+
+			while (valorEntrada == null || valorEntrada != 0) {
+				valorEntrada = GetNumberInput(valorEntrada);
+				while (valorEntrada > 9999 || valorEntrada < -9999) {
+					Console.WriteLine("El numero que ingreso tiene mas de 4 digitos, intente nuevamente");
+					valorEntrada = null;
+					valorEntrada = GetNumberInput(valorEntrada);
+				}
+
+				if (valorEntrada == 0) {
+					continue;
+				}
+
+				if (valorEntrada % 2 == 0) {
+					Console.WriteLine(string.Format("El numero {0} es par", valorEntrada));
+					valorEntrada = null;
+				} else {
+					Console.WriteLine(string.Format("El numero {0} es impar", valorEntrada));
+					valorEntrada = null;
+				}
+			}
+
+			Console.WriteLine("Hasta Luego.");
+		}
+	}
+}
---
+++ cuatrimestre_1/estructura_de_datos/tareas/tarea_n/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace tarea_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enunciado
+
+            // Pedir ingreso de tamano de array
+            Console.Write("\nIngrese el tamano del array: ");
+            int arraySize = Convert.ToInt16(Console.ReadLine());
+
+            // Pedir tipo de impresion
+            Console.Write("\nIngrese el tpo de impresion: (r - raw / f - formatted)");
+            char printType = Console.ReadLine()[0];
+
+            // Inicializar variables
+            int[] arraySolution = new int[arraySize];
+            int maxNumber = 0;
+            string message = "\t";
+            
+            // Inicializar clase Random para generar numeros aleatorios
+            Random randomManager = new Random();
+
+            // Ciclo principal
+            for (int i = 0; i < arraySize; i++) {
+                int newArrayEntry = randomManager.Next(10, 1000);
+                if (newArrayEntry > maxNumber) {
+                    maxNumber = newArrayEntry;
+                }
+                arraySolution[i] = newArrayEntry;
+                message += Convert.ToString(newArrayEntry) + ", ";
+                if ((i + 1) % 10 == 0) {
+                    message += "\n\t";
+                }
+            }
+
+
+            Console.WriteLine(message);
+
+            // Console.WriteLine("El Array queda como: ");
+            // Console.WriteLine("{ " + string.Join(", ", arraySolution) + " }");
+            // Console.WriteLine("El Maximo valor del array es: "+maxNumber);
+        }
+    }
+}
